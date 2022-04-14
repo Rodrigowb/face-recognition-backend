8a1c157fc50577163541a262179acf4cc6589c7b
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2022-04-14 10:58:14 -0400

    creating the expressions router for crud functionalities

---
+++ routes/expressions.js
@@ -0,0 +1,20 @@
+// Import router and controllers
+import { Router } from "express";
+import { expressionsController as controller } from '../controllers/expressions.js'
+
+// Create a new router
+const router = Router()
+
+// Create the middlewares routers
+// POST
+router.post('/expressions', controller.create);
+// GET
+router.get('/expressions', controller.getAll);
+router.get('/expressions/datetime/:datetime', controller.getByDatetime);
+router.get('/expressions/emotion/:emotion', controller.getByEmotion);
+router.checkout('/expressions/userid/:userid', controller.getByUserId);
+// PUT
+router.put('expressions/userid/:userid', controller.updateByUserId);
+// DELETE
+router.delete('/expressions/datetime/:datetime', controller.deleteByDatetime);
+router.delete('/expressions/userid/:userid', controller.deleteByUserId);
